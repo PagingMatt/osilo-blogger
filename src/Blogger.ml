5b81596d320947001f842513c21ebaba9c9d5b3c
Matt Harrison <matt.anthony.harrison@gmail.com>
2017-01-18 17:03:41 +0000

    Spacing

--- src/Blogger.ml
+++ src/Blogger.ml
@@ -22,7 +22,7 @@ let string_member s j =
 let assoc_member s j = 
   match Yojson.Basic.Util.member s j with
   | `Assoc m -> m
-  | _         -> raise (Decoding_failed s)
+  | _        -> raise (Decoding_failed s)
 
 let read_file file =
   let buf = String.make 65536 'x' in
@@ -80,10 +80,10 @@ let update_post_list title id my_peer key =
   >|= (fun l -> 
     let entry = `Assoc ((id,`String title)::l) in
     let plaintext = (`Assoc [("posts/list.json",entry)]) |> Yojson.Basic.to_string |> Cstruct.of_string in
-    let c,i = Cryptography.CS.encrypt' ~key ~plaintext in
-    let body = Coding.encode_client_message ~ciphertext:c ~iv:i in
+    let c',i' = Cryptography.CS.encrypt' ~key ~plaintext in
+    let body' = Coding.encode_client_message ~ciphertext:c' ~iv:i' in
     let path = "/client/set/local/blogger" in
-    Http_client.post ~peer:my_peer ~path ~body
+    Http_client.post ~peer:my_peer ~path ~body:body'
     >|= fun _ -> ())
 
 let publish_post title id post my_peer key =
@@ -151,7 +151,10 @@ let show ~peer =
   >|= (fun (c,b) -> Coding.decode_client_message b) 
   >|= (fun (ciphertext,iv) -> Cryptography.CS.decrypt' ~key ~ciphertext ~iv)
   >|= Cstruct.to_string
-  >|= (fun m -> Printf.printf "All blog posts from %s:\n\n%s\n\n" (Peer.host peer) m)
+  >|= Yojson.Basic.from_string
+  >|= assoc_member "posts/list.json"
+  >|= (fun l -> Printf.printf "Posts from %s:\n" (Peer.host peer);
+    List.iter l ~f:(fun (i',(`String t)) -> Printf.printf "\"%s\" (ID: %s)\n" t i'))
 
 let show_my () = 
   let my_peer,key = read_config () in
@@ -163,7 +166,10 @@ let show_my () =
   >|= (fun (c,b) -> Coding.decode_client_message b) 
   >|= (fun (ciphertext,iv) -> Cryptography.CS.decrypt' ~key ~ciphertext ~iv)
   >|= Cstruct.to_string
-  >|= (fun m -> Printf.printf "All my blog posts:\n\n%s\n\n" m)
+  >|= Yojson.Basic.from_string
+  >|= assoc_member "posts/list.json"
+  >|= (fun l -> Printf.printf "My posts:\n";
+    List.iter l ~f:(fun (i',(`String t)) -> Printf.printf "\"%s\" (ID: %s)\n" t i'))
 
 module Cli = struct 
   let init =
